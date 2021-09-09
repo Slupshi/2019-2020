863b4dab51b400d6a616924ae622a18b4fcb95f0
Slupshi <90383697+Slupshi@users.noreply.github.com>
2021-09-09 20:54:42 +0200

    Add files via upload

--- Juste_Prix.py
+++ Juste_Prix.py
@@ -12,6 +12,8 @@ timer = 60
 
 
 
+
+
 def valider():
 
     global rtent
@@ -38,40 +40,35 @@ def valider():
             rep.set("C'est plus !")
         if k==n:
             rep.set("C'est gagné !")
+            timer = 100
+            label1.config(bg="green")
 
         rtent= rtent-1
         tent.set("Il te reste "+ str(rtent) + " tentatives")
+
+
+
+def start():
+    global timer
+    timer=60
+    triche = 0
+    fenp.after(100, compteur)
 
 def compteur():
     global timer
     global triche
 
-    triche = 0
-    if timer >= 59:
-        rep.set("Choisissez un nombre !")
-        label1.config(bg="snow4")
 
 
-    if timer > 0 :
-        timer = timer-1
+    if 61> timer > 0 :
         t.set("Il te reste "+ str(timer) + " secondes")
+        timer = timer-1
         fenp.after(1000, compteur)
-    if timer == 0 :
+    elif timer == 0 :
         rep.set("Trop tard !")
         label1.config(bg="red3")
         label4.config(bg="red3")
-        fenp.after(100, compteur)
-    if timer < 0:
-        rep.set("Appuyez sur Start")
-        timer = 60
-        n = randint(1, 100)
-        triche = 1
-        rtent = 5
-        label1.config(bg="snow4")
-        t.set("Il te reste "+ str(timer) + " secondes")
-        tent.set("Il te reste "+ str(rtent) + " tentatives")
-        label3.config(bg="snow4")
-        label4.config(bg="snow4")
+
 
 
 
@@ -81,8 +78,19 @@ def reset():
     global rtent
 
 
-    timer = -10
+
     rep.set("Reset en cours")
+    rep.set("Appuyez sur Start")
+    timer = 100
+    n = randint(1, 100)
+    triche = 1
+    rtent = 5
+    label1.config(bg="snow4")
+    t.set("Il te reste x secondes")
+    tent.set("Il te reste "+ str(rtent) + " tentatives")
+    label3.config(bg="snow4")
+    label4.config(bg="snow4")
+
 
 
 
@@ -100,7 +108,7 @@ rep.set("Appuyez sur Start")
 tent=StringVar()
 tent.set("Il te reste "+ str(rtent) + " tentatives")
 t=StringVar()
-t.set("Il te reste "+ str(timer) + " secondes")
+t.set("Il te reste x secondes")
 label0 = Label(fenp, text='Bienvenue sur le célèbre jeu du juste prix, tu dois deviner le prix auquel je pense, il se situe entre 1 et 100', width =100, height=2 , bg='white',relief='solid').grid(row=1,column=2,columnspan=3)
 label1 = Label(fenp, textvariable=rep, width =20, bg='snow4',relief='solid')
 label1.grid(row=2,column=3,sticky=S)
@@ -113,7 +121,7 @@ label11 = Label(fenp, text='', width =10, height=20 , bg='chartreuse3').grid(row
 label12 = Label(fenp, text='', width =10, height=20 , bg='chartreuse3').grid(row=1,column=5,rowspan=5)
 
 bou1 = Button(fenp, text='Valider', width =8, command=valider, bg='snow2',relief='raised').grid(row=3,column=2,columnspan=2,sticky=S)
-bou2 = Button(fenp, text='Start', width =8, command=compteur, bg='snow2',relief='raised').grid(row=4,column=3,columnspan=1,sticky=S)
+bou2 = Button(fenp, text='Start', width =8, command=start, bg='snow2',relief='raised').grid(row=4,column=3,columnspan=1,sticky=S)
 bou3 = Button(fenp, text='Reset', width =8, command=reset, bg='red3',relief='raised').grid(row=5,column=5,columnspan=1)
 Ea=Entry(fenp)
 Ea.grid(row=3,column=3,columnspan=1,sticky=S)
